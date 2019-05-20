30b96a608ef1bc644436bd8f73b5b6e060475fca
epochong <Valentineone@example.com>
2019-05-20 20:29:07 +0800

    Have no girlfriend, i am coding in the school

---
+++ src/Code_01_MaxBottleNum.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author wangchong
+ * @date 2019/5/20 19:49
+ * @email 876459397@qq.com
+ * @CSDN https://blog.csdn.net/wfcn_zyq
+ * @describe
+ */
+public class Code_01_MaxBottleNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = 0;
+        do {
+            n = input.nextInt();
+
+        } while (n != 0);
+    }
+}
